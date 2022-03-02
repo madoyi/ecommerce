5eaf9b97e0eaa897a5d76c2499dbb4b40850d4a8
madoyi <brianmadoyi@gmail.com>
2022-03-02 16:13:07 +0300

    nc: testing views

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
@@ -8,6 +8,9 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = 'categories'
+        
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
         
     def __str__(self):
         return self.name
@@ -30,6 +33,10 @@ class Product(models.Model):
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('-created',)
+        
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+
         
     def __str__(self):
         return self.title
---
+++ store/tests/test_views.py
@@ -0,0 +1,49 @@
+from unittest import skip
+from django.test import TestCase
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from store.models import Category, Product
+from django.urls import reverse
+from store.views import all_products
+from django.test import Client,RequestFactory, TestCase
+
+
+class TestViewResponses(TestCase):
+    def setUp(self):
+        self.c = Client()
+        self.factory = RequestFactory()
+        User.objects.create(username='admin')
+        Category.objects.create(name='django', slug='django')
+        Product.objects.create(category_id=1, title='django', created_by_id=1,
+                               slug='django', price='20.00', image='django')
+     
+    def test_url_allowed_hosts(self):
+        """test allowed hosts
+        """
+        response =self.c.get('/')
+        self.assertEqual(response.status_code, 200)    
+    
+    def test_product_detail_url(self):
+        response = self.c.get(reverse('store:product_detail', args=['django']))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_category_detail_url(self):
+        response = self.c.get(reverse('store:category_list', args=['django']))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_homepage_html(self):
+        request =HttpRequest()
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        print(html)
+        self.assertIn('<title>BookStore</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_view_fuction(self):
+        request = self.factory.get('/item/django')
+        response = all_products(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<title>BookStore</title>', html)
+        self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
+        self.assertEqual(response.status_code, 200)
